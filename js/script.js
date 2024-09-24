0a0bef135d70d1fcc74d32cb1bd80cc0fa7b1623
= <=>
2024-09-24 00:28:33 -0700

    add client files

---
+++ js/script.js
@@ -0,0 +1,21 @@
+fetch("http://localhost:3000/api/fundraisers")
+  .then((response) => response.json())
+  .then((response) => {
+    const fundraisers = response.data.fundraisers;
+    let card = "";
+    fundraisers.forEach((item) => {
+      card += `
+        <div class="card">
+            <div class="img">
+                <img src="./images/crowd.png" width="100px" alt="">
+            </div>
+            <h3>${item.organizer}</h3>
+            <a href='fundraiser.html?crowd-id=${item.fundraiserId}'>View Details</a>
+
+					
+            
+        </div>`;
+    });
+    document.getElementById("card-container").innerHTML = card;
+  })
+  .catch((err) => console.log(err));
